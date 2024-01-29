d228fe985543497f6d3a0b742ddf6388b1a2d408
Jenil Katudia <111152607+jenil2803@users.noreply.github.com>
2024-01-29 17:32:06 +0530

    Add files via upload

--- user.js
+++ user.js
@@ -8,10 +8,12 @@ const app = express();
 const port = 3001;
 
 
-const jwtSecret = crypto.randomBytes(32).toString('hex');
+const jwtSecret = 'jenil';
 
 
 app.use(bodyParser.json());
+
+
 
 const pool = mysql.createPool({
   host: 'localhost',
@@ -23,15 +25,38 @@ const pool = mysql.createPool({
   queueLimit: 0
 });
  
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
 
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized - Token missing' });
+  }
+
+  jwt.verify(token, jwtSecret, { algorithms: ['HS256'] }, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: 'Forbidden - Invalid token' });
+    }
+    req.user = user;
+    next();
+  });
+}
 app.post('/signup', async (req, res) => {
   const { username, password } = req.body;
 
   if (password.length < 8) {
     return res.status(400).json({ error: 'Password must be at least 8 characters long' });
   }
+  const connection = await pool.getConnection();
 
   try {
+    const [duplicate] = await connection.query('SELECT * FROM user_details WHERE username = ?', [username]);
+
+    if (duplicate.length > 0) {
+      return res.status(402).json({ error: 'Username already exists. Please try another username.' });
+    }
+
+  
     const hashPass = crypto.createHash('sha256').update(password).digest('hex');
 
     const newUser = {
@@ -39,10 +64,9 @@ app.post('/signup', async (req, res) => {
       password: hashPass,
     };
 
-    const connection = await pool.getConnection();
 
     try {
-      result = await connection.query('INSERT INTO user_details SET ?', newUser);
+      const result = await connection.query('INSERT INTO user_details SET ?', newUser);
       console.log('Inserted new record with user_id:', result[0].insertId);
       res.status(200).json({ message: 'User signed up successfully!' });
     } finally {
@@ -97,7 +121,10 @@ function generateToken(username) {
       res.status(500).json({ error: 'Internal Server Error' });
     }
   });
-  
+app.get('/data', authenticateToken, (req, res) => {
+  const username = req.user.username;
+    res.json({ message: 'valid user', username });
+});
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
